451c1a4713abebe1d6939b6e05e2f1a64a8b4b24
rchhatbar <1992raj.chhatbar@gmail.com>
2017-05-16 15:23:53 -0400

    adding files

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -1,6 +1,5 @@
 mainObj.controller("mainCtrl", function ($scope, $http) {
     var url = "../Services/json.json";
-    console.log(url);
     $http.get(url).success(function (response) {
         $scope.productList = response.productList;
     });
